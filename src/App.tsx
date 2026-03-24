27398d8bedcbccfd3034f2b37df3ecf836724628
ArunKN <arun.kn@diagnal.com>
2026-03-24 12:48:46 +0530

    implement the ky client

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,33 @@
-
 import { useEffect } from "react";
 import AxiosService from "./Services/AxiosService";
 import GetBenchmarkMetrics from "./Hooks/UseBenchmark";
-
-
+import kyService from "./Services/kyService";
 
 const App = () => {
-  useEffect(()=>{
-    const runAxios=async()=>{
-      const res=await GetBenchmarkMetrics(AxiosService(),{iteration:50,concurrent:10})
-      console.table(res);
-    }
-    runAxios();
-  },[])
-  return (
-    <div>App</div>
-  )
-}
+  const runAxios = async () => {
+    const res = await GetBenchmarkMetrics(AxiosService(), { iteration: 50, concurrent: 10 });
+    console.table(res);
+  };
 
-export default App
+  const runKy = async () => {
+    const res = await GetBenchmarkMetrics(kyService(), { iteration: 50, concurrent: 10 });
+    console.table(res);
+  };
+  // useEffect(()=>{
+  //   const runAxios=async()=>{
+  //     const res=await GetBenchmarkMetrics(AxiosService(),{iteration:50,concurrent:10})
+  //     console.table(res);
+  //   }
+    
+  //   runAxios();
+  //   runKy();
+  // },[])
+  return (
+    <div>
+      <button onClick={runKy}>Ky</button>
+      <button onClick={runAxios}>Axios</button>
+    </div>
+  );
+};
+
+export default App;
