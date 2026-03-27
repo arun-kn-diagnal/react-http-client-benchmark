8b9db496d3a7ff7ebdddbf3aa4025cbc1002b372
ArunKN <arun.kn@diagnal.com>
2026-03-27 08:58:37 +0530

    add the logic for parsing diabled and enabled in redaxios

--- src/Services/RedAxiosService.tsx
+++ src/Services/RedAxiosService.tsx
@@ -1,8 +1,11 @@
-import axios from "redaxios"
- const instance=axios.create({baseURL:"https://api-entertainment-v1.enlight.diagnal.com"})
-const RedAxiosService =async () => {
-   const response=instance.get('/content/filters/KIDS-AND-FAMILY?origin=enhance&origin=vcms&source=enhance&region=IN&maxParentalRatings=UA&language=en-US&platform=web');
+import axios from "redaxios";
+const instance = axios.create({ baseURL: "https://api-entertainment-v1.enlight.diagnal.com" });
+export const RedAxiosService = async () => {
+  const response = instance.get("/content/filters/KIDS-AND-FAMILY?origin=enhance&origin=vcms&source=enhance&region=IN&maxParentalRatings=UA&language=en-US&platform=web", { responseType: "json" });
   return response;
-}
+};
 
-export default RedAxiosService
+export const RedAxiosServiceNoParse = async () => {
+  const response = instance.get("/content/filters/KIDS-AND-FAMILY?origin=enhance&origin=vcms&source=enhance&region=IN&maxParentalRatings=UA&language=en-US&platform=web", { responseType: "text" });
+  return response;
+};
